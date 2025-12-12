36412b5c9e0109c84cce33d19838eb1f469f4c9c
Abdullah881485 <abdullahalasad106882@gmail.com>
2025-12-12 12:37:38 +0600

    payment info api called

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
-// const admin = require("firebase-admin");
+const admin = require("firebase-admin");
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000
-
+const stripe = require('stripe')(process.env.STRIPE_KEY)
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.awdvvgg.mongodb.net/?appName=Cluster0`;
 
 
@@ -20,31 +20,39 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 // });
 
 
+const serviceAccount = require("./fundmate-project-firebase-adminsdk-fbsvc-600454ed5f.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+
 // middleware
 app.use(cors())
 app.use(express.json())
 
-// const verifyFirebaseToken = async (req, res, next) => {
-//     const authorization = req.headers.authorization
-//     if (!authorization) {
-//         return res.status(401).send({ message: 'Unauthorized access' })
-//     }
-//     const token = authorization.split(' ')[1]
 
-//     if (!token) {
-//         return res.status(401).send({ message: 'Unauthorized access' })
-//     }
+const verifyFirebaseToken = async (req, res, next) => {
+    const authorization = req.headers.authorization
+    if (!authorization) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    const token = authorization.split(' ')[1]
+
+    if (!token) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
 
 
-//     try {
-//         const decoded = await admin.auth().verifyIdToken(token)
-//         req.token_email = decoded.email;
-//         next()
-//     } catch {
-//         return res.status(401).send({ message: 'Unauthorized access' })
-//     }
+    try {
+        const decoded = await admin.auth().verifyIdToken(token)
+        req.token_email = decoded.email;
+        next()
+    } catch {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
 
-// }
+}
 
 
 const client = new MongoClient(uri, {
@@ -67,6 +75,7 @@ async function run() {
         const loanCollection = db.collection('allLoan')
         const applicationCollection = db.collection('allApplication')
         const userCollection = db.collection('allUsers')
+        const paymentCollection = db.collection('payment')
 
         app.post("/allLoan", async (req, res) => {
             const newLoan = req.body;
@@ -99,6 +108,7 @@ async function run() {
             res.send(result)
         })
         app.get("/availableLoan", async (req, res) => {
+
             const cursor = loanCollection.find({ showOnHome: true }).limit(6);
             const result = await cursor.toArray();
             res.send(result);
@@ -122,9 +132,13 @@ async function run() {
             res.send(result);
         });
         app.get("/allLoan", async (req, res) => {
-            const cursor = loanCollection.find();
+            const { limit = 0, skip = 0 } = req.query
+            // console.log(req.headers);
+
+            const cursor = loanCollection.find().limit(Number(limit)).skip(Number(skip));
             const result = await cursor.toArray();
-            res.send(result);
+            const total = await loanCollection.countDocuments()
+            res.send({ result, total: total });
         });
 
         app.get("/loanDetails/:id", async (req, res) => {
@@ -133,7 +147,7 @@ async function run() {
             const result = await loanCollection.findOne(query)
             res.send(result)
         })
-        app.get("/users", async (req, res) => {
+        app.get("/users", verifyFirebaseToken, async (req, res) => {
             try {
                 const page = parseInt(req.query.page) || 1;
                 const limit = parseInt(req.query.limit) || 10;
@@ -180,13 +194,13 @@ async function run() {
             const result = await userCollection.updateOne(query, update)
             res.send(result)
         })
-        app.get('/users/:email/role', async (req, res) => {
+        app.get('/users/:email/role', verifyFirebaseToken, async (req, res) => {
             const email = req.params.email;
             const query = { email }
             const user = await userCollection.findOne(query);
             res.send({ role: user?.role || 'Borrower' })
         })
-        app.get("/allApplication", async (req, res) => {
+        app.get("/allApplication", verifyFirebaseToken, async (req, res) => {
             const cursor = applicationCollection.find()
             const result = await cursor.toArray();
             res.send(result);
@@ -197,7 +211,7 @@ async function run() {
             const result = await applicationCollection.deleteOne(query)
             res.send(result)
         })
-        app.get("/statusByApplication", async (req, res) => {
+        app.get("/statusByApplication", verifyFirebaseToken, async (req, res) => {
             const status = req.query.status
             const query = {}
             if (status) {
@@ -217,32 +231,106 @@ async function run() {
             const result = await applicationCollection.updateOne(query, update)
             res.send(result)
         })
-        app.post("/allApplication", async (req, res) => {
+        app.post("/allApplication", verifyFirebaseToken, async (req, res) => {
             const newApplication = req.body;
             const result = await applicationCollection.insertOne(newApplication)
             res.send(result)
         })
 
-        app.get("/manageLoan", async (req, res) => {
+        app.get("/manageLoan", verifyFirebaseToken, async (req, res) => {
             const createdBy = req.query.email;
             const query = {}
             if (createdBy) {
                 query.createdBy = createdBy
-                // if (req.token_email !== email) {
-                //     return res.status(403).send({ message: 'Forbidden access' })
-                // }
+                if (req.token_email !== createdBy) {
+                    return res.status(403).send({ message: 'Forbidden access' })
+                }
             }
             const cursor = loanCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         });
 
-        app.get('/myLoanApplication', async (req, res) => {
-            const email = req.query.email;
-            const query = { email }
-            const cursor = applicationCollection.find(query)
-            const result = await cursor.toArray()
-            res.send(result)
+        
+
+
+
+        // Payment related API
+
+        app.post('/applicationFee', async (req, res) => {
+            const paymentInfo = req.body;
+            const session = await stripe.checkout.sessions.create({
+                line_items: [
+                    {
+
+                        price_data: {
+                            currency: 'USD',
+                            unit_amount: 1000,
+                            product_data: {
+                                name: paymentInfo.loanTitle,
+                            }
+
+                        },
+                        quantity: 1,
+                    },
+                ],
+                customer_email: paymentInfo.email,
+                mode: 'payment',
+                metadata: {
+                    loanId: paymentInfo.loanId,
+                    loanName: paymentInfo.loanTitle,
+                },
+                success_url: `${process.env.CLIENT_DOMAIN}/dashboard-layout/success?session_id={CHECKOUT_SESSION_ID}`,
+                cancel_url: `${process.env.CLIENT_DOMAIN}/dashboard-layout/myLoans`,
+            });
+
+            res.send({ url: session.url })
+        });
+
+        app.patch('/paymentSuccess', async (req, res) => {
+            const session_id = req.query.session_id
+            // console.log(session_id);
+            const session = await stripe.checkout.sessions.retrieve(session_id)
+            // console.log(session);
+            if (session.payment_status === 'paid') {
+                const loanId = session.metadata.loanId
+                const query = { _id: new ObjectId(loanId) }
+                const update = {
+                    $set: {
+                        feeStatus: 'paid',
+
+                    }
+                }
+                const result = await applicationCollection.updateOne(query, update)
+                const paymentInfo = {
+                    fee: session.amount_total / 100,
+                    email: session.customer_email,
+                    loanId: session.metadata.loanId,
+                    loanTitle: session.metadata.loanName,
+                    transactionId: session.payment_intent,
+                    feeStatus: session.payment_status,
+                }
+
+                if (session.payment_status === 'paid') {
+                    const paymentResult = await paymentCollection.insertOne(paymentInfo)
+                    res.send({ success: true, modifyApplication: result, paymentInfo: paymentResult })
+                }
+            }
+
+
+
+            res.send({ success: true })
+        })
+        app.get("/payment", async (req, res) => {
+            console.log(req.headers);
+
+            const loanId = req.query.id
+            const query = {}
+            if (loanId) {
+                query.loanId = loanId
+            }
+            const result = await paymentCollection.findOne(query)
+            res.send(result);
         })
 
         // await client.db("admin").command({ ping: 1 });
